3c3e3211f42fc99f38c4a94926ee278bb70a4519
jeelpatel633 <115190028+jeelpatel633@users.noreply.github.com>
2024-07-20 14:44:51 -0400

    Create MainNode.js

---
+++ MainNode.js
@@ -0,0 +1,14 @@
+console.log("js node file created successfully");
+console.log("here is new jenkins chnages");
+
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+let temp;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
