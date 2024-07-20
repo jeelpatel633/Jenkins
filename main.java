2a910bcc749928b3c11453e3c82847c9d502f88d
jeelpatel633 <115190028+jeelpatel633@users.noreply.github.com>
2024-07-20 14:27:41 -0400

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class main{
-
-    public static void main(String[] args) {
-		System.out.println("hello World");
-		System.out.println("Check Date: "+ new Date());
-	}
-}
