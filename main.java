e6200c7d6bc17969a06298d23953152ddbe81f22
jeelpatel633 <115190028+jeelpatel633@users.noreply.github.com>
2024-07-20 14:39:24 -0400

    Update main.java

--- main.java
+++ main.java
@@ -5,5 +5,15 @@ public class main{
     public static void main(String[] args) {
 		System.out.println("hello World");
 		System.out.println("Check Date: "+ new Date());
+		System.out.println("checking new commit for jenkins");
+		System.out.println("Check Date: "+ new Date());
+		int i,fact=1;  
+ 		 int number=5;//It is the number to calculate factorial    
+  		for(i=1;i<=number;i++){    
+      		fact=fact*i;    
+  		}    
+  		System.out.println("Factorial of "+number+" is: "+fact);    
+ 		
+	    
 	}
 }
