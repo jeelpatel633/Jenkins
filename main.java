e16b576e27fa561a90fa849f91cbca39c9d297fc
jeelpatel633 <115190028+jeelpatel633@users.noreply.github.com>
2024-07-20 14:28:38 -0400

    Initial commit
    jenkins commit

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class main{
+
+    public static void main(String[] args) {
+		System.out.println("hello World");
+		System.out.println("Check Date: "+ new Date());
+	}
+}
